1194a133cefe77335d5a5c472d312f00673409a1
HAMADI25 <hamadiyasmine35@gmail.com>
2024-10-25 16:44:18 +0200

    TP6

--- tp6/meteo-angular/src/app/meteo-detail/meteo-detail.component.ts
+++ tp6/meteo-angular/src/app/meteo-detail/meteo-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { MeteoService } from "../services/meteo.service";
-
 @Component({
   selector: "app-meteo-detail",
   templateUrl: "./meteo-detail.component.html",
@@ -10,22 +9,17 @@ import { MeteoService } from "../services/meteo.service";
 export class MeteoDetailComponent implements OnInit {
   meteo: any;
   latlon: string = "";
-
+  fiveDayForecast: any[] = [];
   constructor(
     private route: ActivatedRoute,
     private meteoService: MeteoService
   ) {}
-
   ngOnInit() {
     this.getMeteo();
+    this.getFiveDayForecast();
   }
-
   getMeteo(): void {
-    // pour lire la paramètre 'name' dans l'URL de la page  comme définit dans le router avec
-    // path: 'meteo/:name'
     const name = this.route.snapshot.paramMap.get("name");
-
-    console.log("getmeteo pour", name);
     if (name) {
       this.meteoService
         .getMeteo(name)
@@ -34,6 +28,25 @@ export class MeteoDetailComponent implements OnInit {
           this.latlon = `${this.meteo.coord.lat},${this.meteo.coord.lon}`;
         })
         .catch((fail) => (this.meteo = fail));
+    }
+  }
+  getFiveDayForecast(): void {
+    const name = this.route.snapshot.paramMap.get("name");
+    if (name) {
+      this.meteoService
+        .getFiveDayForecast(name)
+        .then((response) => {
+          // Filtrer pour obtenir une prévision par jour à midi
+          this.fiveDayForecast = response.list.filter((item: any) =>
+            item.dt_txt.includes("12:00:00")
+          );
+        })
+        .catch((error) =>
+          console.error(
+            "Erreur lors de la récupération des prévisions sur 5 jours :",
+            error
+          )
+        );
     }
   }
 }
